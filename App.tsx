ac458f1b6cc8dd8b9d5f9938a7b262aeb0e3b6ec
Kwonkunkun <kunwoo5012@naver.com>
2022-06-06 21:04:45 +0900

    ➕ plus: add storybook dependency & setting

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar, useColorScheme, LogBox, Platform } from "react-native";
 import SplashScreen from "react-native-splash-screen";
 /**
  * ? Local Imports
  */
 import Navigation from "./src/navigation";
+import StorybookUIRoot from "./.storybook/Storybook";
 
 LogBox.ignoreAllLogs();
 
+// const IS_STORY_BOOK_MODE = true;
+const IS_STORY_BOOK_MODE = false;
+
 const App = () => {
+  return (
+    <React.Fragment>
+      {IS_STORY_BOOK_MODE ? <StoryBookRoot /> : <DefaultRoot />}
+    </React.Fragment>
+  );
+};
+
+const DefaultRoot = () => {
   const scheme = useColorScheme();
   const isDarkMode = scheme === "dark";
 
@@ -19,17 +31,26 @@ const App = () => {
       StatusBar.setBackgroundColor("rgba(0,0,0,0)");
       StatusBar.setTranslucent(true);
     }
+    SplashScreen.hide();
 
-    setTimeout(() => {
-      SplashScreen.hide();
-    }, 750);
+    // //여기 추후에 setting 다되면 splash hide되게 수정
+    // setTimeout(() => {
+    //   SplashScreen.hide();
+    // }, 750);
   }, [scheme, isDarkMode]);
-
   return (
     <>
       <Navigation />
     </>
   );
+};
+
+// storybook일 경우
+const StoryBookRoot = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return <StorybookUIRoot />;
 };
 
 export default App;
